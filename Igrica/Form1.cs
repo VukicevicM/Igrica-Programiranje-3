eece74dfc8f8d122fc76546e24017fa76223644c
VukicevicM <mvukicevic02@gmail.com>
2025-06-21 18:57:50 +0200

    Ozadje se premika glede na pozicijo macke.

--- Igrica/Form1.cs
+++ Igrica/Form1.cs
@@ -18,6 +18,10 @@ namespace Igrica
         Timer timer = new Timer();
         bool levo, desno;
 
+        // Ozadje se premika, ko mačka prečka sredino
+        int premikOzadja = 0;
+        int mejaKamere = 400; // polovica širine okna
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +34,13 @@ namespace Igrica
 
             // Dodamo tla kot blok
             bloki.Add(new Blok(0, 500, 800, 100));
+
+            // Dodamo nekaj platform za skakanje
+            bloki.Add(new Blok(300, 400, 100, 20));
+            bloki.Add(new Blok(500, 350, 100, 20));
+            bloki.Add(new Blok(700, 300, 100, 20));
+            bloki.Add(new Blok(900, 250, 100, 20));
+            bloki.Add(new Blok(1100, 200, 100, 20));
 
             // Nastavimo timer za glavno zanko igre
             timer.Interval = 20;
@@ -44,27 +55,68 @@ namespace Igrica
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // Premikanje levo/desno
+            // Mačka se premika levo/desno
             if (levo) macka.PremakniLevo();
             if (desno) macka.PremakniDesno();
+
+            // Kamera se premakne, če mačka prečka polovico zaslona v desno
+            if (macka.Obmocje.X > mejaKamere)
+            {
+                int dx = macka.Obmocje.X - mejaKamere;
+                macka.Obmocje.X = mejaKamere;
+                premikOzadja -= dx;
+                foreach (var b in bloki)
+                    b.Obmocje.X -= dx;
+            }
+            // Kamera se premakne, če mačka gre levo mimo meje kamere
+            else if (macka.Obmocje.X < 100 && premikOzadja < 0)
+            {
+                int dx = 100 - macka.Obmocje.X;
+                macka.Obmocje.X = 100;
+                premikOzadja += dx;
+                foreach (var b in bloki)
+                    b.Obmocje.X += dx;
+            }
 
             // Posodobimo pozicijo glede na gravitacijo
             macka.Posodobi();
 
-            // Detekcija trka z bloki (npr. tla)
+            // Detekcija trka z bloki
             macka.NaTleh = false;
             foreach (var blok in bloki)
             {
                 Rectangle b = blok.Obmocje;
                 Rectangle m = macka.Obmocje;
 
-                // Trk od zgoraj: mačka ne sme pasti skozi tla
-                if (m.IntersectsWith(b) && m.Bottom <= b.Top + 10 && macka.HitrostY >= 0)
+                // Trk od zgoraj (padanje na platformo)
+                if (m.IntersectsWith(b) && m.Bottom <= b.Top + macka.HitrostY && macka.HitrostY >= 0)
                 {
                     macka.Obmocje.Y = b.Top - m.Height;
                     macka.HitrostY = 0;
                     macka.NaTleh = true;
                 }
+                // Trk s spodnje strani (udarjanje v spodnji del bloka)
+                else if (m.IntersectsWith(b) && m.Top >= b.Bottom - macka.HitrostY && macka.HitrostY < 0)
+                {
+                    macka.Obmocje.Y = b.Bottom;
+                    macka.HitrostY = 0; // začne padati
+                }
+                // Trk z leve ali desne (vodoravna blokada)
+                else if (m.IntersectsWith(b))
+                {
+                    if (macka.Obmocje.X < b.X)
+                        macka.Obmocje.X = b.X - m.Width;
+                    else if (macka.Obmocje.X > b.X)
+                        macka.Obmocje.X = b.Right;
+                }
+            }
+
+            // Preverimo če je mačka padla izven zaslona
+            if (macka.Obmocje.Y > this.Height)
+            {
+                timer.Stop();
+                MessageBox.Show("Mačka je padla! Konec igre.");
+                Application.Exit();
             }
 
             Invalidate(); // Povzroči ponovno risanje
@@ -73,12 +125,14 @@ namespace Igrica
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.Clear(Color.LightSkyBlue); // Ozadje
 
-            // Narišemo mačko (krog ali slika)
+            // Ponovi nebo kot ozadje (v prihodnje lahko nadomestiš s sliko ozadja)
+            g.Clear(Color.LightSkyBlue);
+
+            // Nariši mačko (trenutno oranžen krog)
             macka.Narisi(g);
 
-            // Narišemo vse bloke (tla ipd.)
+            // Nariši vse platforme
             foreach (var b in bloki)
                 g.FillRectangle(Brushes.Green, b.Obmocje);
         }
